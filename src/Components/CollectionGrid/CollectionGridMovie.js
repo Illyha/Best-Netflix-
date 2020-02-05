ade18505255a95e895737f05487fcab6bec906ae
Illyha <Illyha@Illyha.local>
2020-02-05 13:59:48 +0200

    added changes with movie

--- src/Components/CollectionGrid/CollectionGridMovie.js
+++ src/Components/CollectionGrid/CollectionGridMovie.js
@@ -1,9 +1,7 @@
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { compose } from "redux";
-
 import { selectIsMovieFetching } from "../../Redux/Movie/movie-selectors";
-
 import WithSpinnerMovie from "../WithSpinner/WithSpinnerMovie";
 import CollectionGrid from "./CollectionGrid";
 
