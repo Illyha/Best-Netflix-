2b9d40b4b0cd186881ffe59f4dda49e8b7764411
Illyha <Illyha@Illyha.local>
2020-02-05 14:06:38 +0200

    changes added in code

--- src/Components/CollectionGridItem/CollectionGridItem.js
+++ src/Components/CollectionGridItem/CollectionGridItem.js
@@ -10,9 +10,9 @@ const CollectionGridItem = ({ item }) => {
       <img src={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${backdrop_path}`} alt="movie" className="grid-item__img"/>
       <img src={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${poster_path}`} alt="movie" className="grid-item__img grid-item__mobile-img"/>
       <div className={`${title ? "grid-item__movie-text" : "grid-item__tv-text"}`}>
-        <h1 className="grid-item__title">{title}</h1>
-        <h1 className="grid-item__title">{name}</h1>
-        <span className="grid-item__overview">{overview}</span>
+        <h1 className="grid-item__title">{title} </h1>
+        <h1 className="grid-item__title">{name} </h1>
+        <span className="grid-item__overview">{overview} </span>
         <AddToList item={item} />
       </div>
     </div>
