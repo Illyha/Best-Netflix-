38610847b03b58069b39711a7c4c352a540357da
Illyha <Illyha@Illyha.local>
2020-02-09 12:55:05 +0200

    added changes in forminput and custombutton

--- src/Pages/SignIn/SignIn.js
+++ src/Pages/SignIn/SignIn.js
@@ -36,55 +36,27 @@ class SignIn extends React.Component {
   render() {
     return (
       <div className="signin">
-        <div
-          className="signin__bg"
-          style={{ backgroundImage: `url(${Background})` }}
-        />
+        <div className="signin__bg" style={{ backgroundImage: `url(${Background})` }}/>
         <div className="signin__container">
           <div className="signin__shadow">
             <h1 className="signin__title">Sign In</h1>
 
-            <form
-              action="POST"
-              autoComplete="new-password"
-              onSubmit={this.handleSubmit}
-            >
-              <FormInput
-                name="email"
-                type="email"
-                value={this.state.email}
-                handleChange={this.handleChange}
-                label="Email"
-                required
-              />
+            <form action="POST" autoComplete="new-password" onSubmit={this.handleSubmit}>
+              <FormInput name="email" type="email" value={this.state.email} handleChange={this.handleChange} label="Email" required/>
 
-              <FormInput
-                name="password"
-                type="password"
-                value={this.state.password}
-                handleChange={this.handleChange}
-                label="Password"
-                required
-              />
+              <FormInput name="password" type="password" value={this.state.password}  handleChange={this.handleChange} label="Password" required/>
 
               <div className="signin__btn-container">
                 <div className="signin__btn">
-                  <CustomButton type="submit" signin>
-                    {" "}
-                    Sign In{" "}
-                  </CustomButton>
+                  <CustomButton type="submit" signin>{" "}Sign In{" "}</CustomButton>
                   <CustomButton onClick={signInWithGoogle}>
-                    <FontAwesomeIcon icon={faGoogle} className="signin__google-icon" />
-                    Sign In With Google
-                  </CustomButton>
+                    <FontAwesomeIcon icon={faGoogle} className="signin__google-icon" />Sign In With Google</CustomButton>
                 </div>
               </div>
             </form>
             <div className="signin__option">
               <span className="signin__option--newuser">New to Netflix?</span>
-              <Link to="/signup" className="signin__option--link">
-                Sign up now.
-              </Link>
+              <Link to="/signup" className="signin__option--link">Sign up now.</Link>
             </div>
           </div>
         </div>
